f197382c7c135e84a2030af62807836e801648e7
playwithsakthigaming-sketch <playwithsakthigaming@gmail.com>
2026-01-23 08:15:38 +0530

    backup.py

--- utils/backup.py
+++ utils/backup.py
@@ -5,5 +5,10 @@ import os
 def backup_db():
     if not os.path.exists("bot.db"):
         return
-    ts = time.strftime("%Y%m%d_%H%M")
-    shutil.copyfile("bot.db", f"backup_bot_{ts}.db")
+
+    os.makedirs("db_backups", exist_ok=True)
+
+    ts = time.strftime("%Y%m%d_%H%M%S")
+    backup_path = f"db_backups/bot_{ts}.db"
+
+    shutil.copyfile("bot.db", backup_path)
